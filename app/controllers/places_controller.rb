1e01032ba2821ce72dd59b61f97c74c50b1515ba
Bryan Roof <broof716@gmail.com>
2019-05-11 19:34:40 +0000

    worked on Nomster part of lesson 21

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
