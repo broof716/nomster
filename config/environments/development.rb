a2750d01d7f7f21c0d8aa8e8639b354968a69115
Bryan Roof <broof716@gmail.com>
2019-05-19 23:50:21 +0000

    finished with Lesson 43

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  #config.action_mailer.smtp_settings = {
+    #address: "smtp.gmail.com",
+    #port: 587,
+    #domain: "example.com",
+    #authentication: "plain",
+    #enable_starttls_auto: true,
+    #user_name: ENV['GMAIL_ADDRESS'],
+    #password: ENV['GMAIL_PASSWORD']
+    #}
 
   
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
@@ -19,6 +19,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+
+  config.action_mailer.delivery_method = :test
 
   # Do not eager load code on boot.
   config.eager_load = false
