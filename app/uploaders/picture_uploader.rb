1f4ea8f6d6b15d9ad081a5533d6e3edc1c08fdd8
Bryan Roof <broof716@gmail.com>
2019-05-21 21:23:18 +0000

    rake reverted db rollback

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,8 +1,7 @@
-class PhotoUploader < CarrierWave::Uploader::Base
+class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  
 
   # Choose what kind of storage to use for this uploader:
   storage :file
