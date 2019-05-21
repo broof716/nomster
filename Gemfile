a0d8c765adc75bf362d63bc899c035d7fcdebd0e
Bryan Roof <broof716@gmail.com>
2019-05-21 23:10:25 +0000

    user dashboard page

--- Gemfile
+++ Gemfile
@@ -76,4 +76,3 @@ gem "font-awesome-rails"
 gem 'devise'
 gem "geocoder"
 gem "figaro"
-gem 'carrierwave', '~> 1.0'
--- db/migrate/20190521205420_add_picture_to_photo.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToPhoto < ActiveRecord::Migration[5.2]
-  def change
-    #add_column :users, :picture, :string
-  end
-end
